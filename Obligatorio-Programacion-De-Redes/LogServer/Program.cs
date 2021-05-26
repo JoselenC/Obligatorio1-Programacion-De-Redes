4268e6f929ebc6eeb65735d14bb5b26b7b2467cb
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-25 22:46:48 -0300

    Se agrega la estructura base del logerver

---
+++ Obligatorio-Programacion-De-Redes/LogServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

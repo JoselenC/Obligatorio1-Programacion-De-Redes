cf1db16d45fce99b39e1dd6941b26f16140a59f7
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-01 21:59:33 -0300

    Se crea el proyecto WebAPI

---
+++ Obligatorio-Programacion-De-Redes/WebAPI/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using GrpcServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("posts")]
+   
+    public class PostController:ControllerBase
+    {
+        private IPostServiceGrpc _postServiceGrpc;
+
+        PostController(IPostServiceGrpc postServiceGrpc)
+        {
+            _postServiceGrpc = postServiceGrpc;
+        }
+
+        [HttpPost]
+        public IActionResult AddPost([FromBody] Post post)
+        {
+            _postServiceGrpc.AddPostAsyc(post);
+            return Ok(post);
+        }
+    }
+}

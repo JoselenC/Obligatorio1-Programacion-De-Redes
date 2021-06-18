3448728b6a73759a4258e3e171304311b0b21b52
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-17 23:14:17 -0300

    Se agrega LogsServer y LogsServerInterface

---
+++ Obligatorio-Programacion-De-Redes/LogsServer/LogService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BusinessLogic.Managers;
+using DomainObjects;
+using LogsServerInterface;
+
+namespace LogsServer
+{
+    public class LogService : ILogService
+    {
+        private ManagerLogRepository _managerLogRepository;
+        
+        public LogService(ManagerLogRepository managerLogRepository)
+        {
+            _managerLogRepository = managerLogRepository;
+           
+        }
+
+        public void AddLog(string log)
+        {
+            Log logToAdd = new Log() {Message = log};
+            _managerLogRepository.Logs.Add(logToAdd);
+        }
+
+        public List<Log> Get()
+        {
+           return _managerLogRepository.Logs.Get();
+        }
+    }
+}
